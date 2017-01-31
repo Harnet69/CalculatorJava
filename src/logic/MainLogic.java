ba8742bd093cc5ebb880c4c65f81df517e3a5ebc
Harnet69 <motobycom@tut.by>
2017-01-31 09:44:23 +0300

    Aded arithmetical calculate mechanism

--- src/logic/MainLogic.java
+++ src/logic/MainLogic.java
@@ -10,9 +10,11 @@ import java.awt.event.ActionListener;
  * Listeners and actions for main frame
  */
 public class MainLogic {
-    private boolean choose = false;private boolean clickIsEqual = false;
+    private boolean choose = false;
+    private boolean clickIsEqual = false;
     private String steat1, steat2, answerSteat; // first number, second number
     private double answer = 0.0;
+    private String strAnswer;
     char oper = ' ';
     private JButton one, two, three, four, five, six, seven, eight, nine, zero, plus, minus, mult, divide, remainder, equal;
     private JTextField textField;
@@ -38,12 +40,22 @@ public class MainLogic {
         remainder = calc.getRemainder();
         equal = calc.getEqual();
         textField = calc.getTextField();
-        one.addActionListener(numb); two.addActionListener(numb); three.addActionListener(numb);
-        four.addActionListener(numb);five.addActionListener(numb);six.addActionListener(numb);
-        seven.addActionListener(numb);eight.addActionListener(numb);nine.addActionListener(numb);
+        one.addActionListener(numb);
+        two.addActionListener(numb);
+        three.addActionListener(numb);
+        four.addActionListener(numb);
+        five.addActionListener(numb);
+        six.addActionListener(numb);
+        seven.addActionListener(numb);
+        eight.addActionListener(numb);
+        nine.addActionListener(numb);
         zero.addActionListener(numb);
-        plus.addActionListener(act); minus.addActionListener(act); mult.addActionListener(act);
-        divide.addActionListener(act); remainder.addActionListener(act);equal.addActionListener(act);
+        plus.addActionListener(act);
+        minus.addActionListener(act);
+        mult.addActionListener(act);
+        divide.addActionListener(act);
+        remainder.addActionListener(act);
+        equal.addActionListener(act);
 
     }
 
@@ -52,20 +64,19 @@ public class MainLogic {
             JButton src = (JButton) event.getSource();
 
             // if button "1" is pressed
-            if(src.equals(one)){
+            if (src.equals(one)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "1";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "1";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "1";
                     }
                     //if "1" are't first
@@ -75,20 +86,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(two)){
+            if (src.equals(two)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "2";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "2";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "2";
                     }
                     //if "1" are't first
@@ -98,20 +108,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(three)){
+            if (src.equals(three)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "3";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "3";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "3";
                     }
                     //if "1" are't first
@@ -121,20 +130,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(four)){
+            if (src.equals(four)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "4";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "4";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "4";
                     }
                     //if "1" are't first
@@ -144,20 +152,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(five)){
+            if (src.equals(five)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "5";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "5";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "5";
                     }
                     //if "1" are't first
@@ -167,20 +174,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(six)){
+            if (src.equals(six)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "6";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "6";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "6";
                     }
                     //if "1" are't first
@@ -190,20 +196,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(seven)){
+            if (src.equals(seven)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "7";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "7";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "7";
                     }
                     //if "1" are't first
@@ -213,20 +218,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(eight)){
+            if (src.equals(eight)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "8";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "8";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "8";
                     }
                     //if "1" are't first
@@ -236,20 +240,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(nine)){
+            if (src.equals(nine)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "9";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "9";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "9";
                     }
                     //if "1" are't first
@@ -259,20 +262,19 @@ public class MainLogic {
                 }
             }
 
-            if(src.equals(zero)){
+            if (src.equals(zero)) {
                 // if it is first operation (not any operation yet)
-                if(clickIsEqual == false){
+                if (clickIsEqual == false) {
                     // if not another statement
-                    if(steat1 == null){
+                    if (steat1 == null) {
                         steat1 = "0";
                     }
                     //if "1" are't first
                     else {
                         steat1 = steat1 + "0";
                     }
-                }
-                else{
-                    if(steat2 == null){
+                } else {
+                    if (steat2 == null) {
                         steat2 = "0";
                     }
                     //if "1" are't first
@@ -281,89 +283,109 @@ public class MainLogic {
                     }
                 }
             }
-            if(clickIsEqual == false){
+            if (clickIsEqual == false) {
                 if (choose == false) {
                     textField.setText(steat1);
                 }
-            }
-            else{
+            } else {
                 textField.setText(steat2);
             }
         }
     }
 
-    public class ActionButtons implements ActionListener{
+    public class ActionButtons implements ActionListener {
 
         public void actionPerformed(ActionEvent event) {
             JButton src = (JButton) event.getSource();
 
             // Plus operation
-            if(src.equals(plus)){
-                if(steat1 == null){
+            if (src.equals(plus)) {
+                if (steat1 == null) {
                     System.out.print("You must add number first");
-                }
-                else if (steat1 != null && steat2 == null){
+                } else if (steat1 != null && steat2 == null) {
                     clickIsEqual = true;
                     oper = '+';
-                }
-                else if(steat1 !=null && steat2 != null){
+                } else if (steat1 != null && steat2 != null) {
                     System.out.print("This is freeware version, calculator supported only two operation!");
                 }
             }
 
             // Minus operation
-            if(src.equals(minus)){
-                if(steat1 == null){
+            if (src.equals(minus)) {
+                if (steat1 == null) {
                     System.out.print("You must add number first");
-                }
-                else if (steat1 != null && steat2 == null){
+                } else if (steat1 != null && steat2 == null) {
                     clickIsEqual = true;
                     oper = '-';
-                }
-                else if(steat1 !=null && steat2 != null){
+                } else if (steat1 != null && steat2 != null) {
                     System.out.print("This is basic version calculator, supported only two operation!");
                 }
             }
 
             // Mult operation
-            if(src.equals(mult)){
-                if(steat1 == null){
+            if (src.equals(mult)) {
+                if (steat1 == null) {
                     System.out.print("You must add number first");
-                }
-                else if (steat1 != null && steat2 == null){
+                } else if (steat1 != null && steat2 == null) {
                     clickIsEqual = true;
                     oper = '*';
-                }
-                else if(steat1 !=null && steat2 != null){
+                } else if (steat1 != null && steat2 != null) {
                     System.out.print("This is basic version calculator, supported only two operation!");
                 }
             }
 
             // Divide operation
-            if(src.equals(divide)){
-                if(steat1 == null){
+            if (src.equals(divide)) {
+                if (steat1 == null) {
                     System.out.print("You must add number first");
-                }
-                else if (steat1 != null && steat2 == null){
+                } else if (steat1 != null && steat2 == null) {
                     clickIsEqual = true;
                     oper = '/';
-                }
-                else if(steat1 !=null && steat2 != null){
+                } else if (steat1 != null && steat2 != null) {
                     System.out.print("This is basic version calculator, supported only two operation!");
                 }
             }
 
             // Divide operation
-            if(src.equals(remainder)){
-                if(steat1 == null){
+            if (src.equals(remainder)) {
+                if (steat1 == null) {
                     System.out.print("You must add number first");
-                }
-                else if (steat1 != null && steat2 == null){
+                } else if (steat1 != null && steat2 == null) {
                     clickIsEqual = true;
                     oper = '%';
-                }
-                else if(steat1 !=null && steat2 != null){
+                } else if (steat1 != null && steat2 != null) {
                     System.out.print("This is basic version calculator, supported only two operation!");
+                }
+            }
+
+            // Equal operation
+            if (src.equals(equal)) {
+                if (steat1 == null) {
+                    System.out.print("You must add number first");
+                } else if (steat1 != null && steat2 == null) {
+                    System.out.print("You must both number first");
+                } else if (steat1 != null && steat2 != null) {
+                    double d1 = 0.0, d2 = 0.0;
+                    // Convert string value to the double
+                    d1 = Double.parseDouble(steat1);
+                    d2 = Double.parseDouble(steat2);
+
+                    switch(oper){
+                        case '+':
+                            answer = d1 + d2; break;
+                        case '-':
+                            answer = d1 - d2; break;
+                        case '/':
+                            answer = d1 / d2; break;
+                        case '*':
+                            answer = d1 * d2; break;
+                        case '%':
+                            answer = d1 % d2; break;
+                    }
+
+                    // Convert double format answer to string for display in textField
+                    strAnswer = Double.toString(answer);
+                    textField.setText(strAnswer);
                 }
             }
         }
